b9b6ca850e21318f03ef5a19938bb954f7e3714d
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2023-01-27 09:41:22 +0100

    Click Sound

--- DreaTextStudie/automotive/src/main/java/com/example/drea_text_studie/ui/studie/words.kt
+++ DreaTextStudie/automotive/src/main/java/com/example/drea_text_studie/ui/studie/words.kt
@@ -1,6 +1,13 @@
 package com.example.drea_text_studie.ui.studie
 
 val allWordsList: List<String> = listOf(
-    "Nick",
-    "Luca"
+    "51674 Wiehl",
+    "02262 Wiehl",
+    "Auto",
+    "Navi",
+    "Spotify",
+    "Links",
+    "Rechts",
+    "Strasse",
+    "53111 Bonn",
 )

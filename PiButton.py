1821f278773d3d4d6f51b901db373a60689ea678
Nick Crisci <nick_gaetano.crisci@smail.th-koeln.de>
2023-02-09 09:37:12 +0100

    Erster Analyse Code, logToJson obsolet

--- PiButton.py
+++ PiButton.py
@@ -6,8 +6,6 @@ button = 3
 
 grovepi.pinMode(button, "INPUT")
 
-old, current = 0, 0, 0
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     client.subscribe("drea")
@@ -25,12 +23,13 @@ client.connect("localhost", 1883, 60)
 client.loop_start()
 
 data = "0,-1,0,0"
-
+old, current = 0, 0
+pressed = 1
 while True:
     try:
         current = grovepi.digitalRead(button)
         if current != old:
-            if current == 1:
+            if current == pressed:
                 client.publish("drea", data)
             old = current
     except IOError:
